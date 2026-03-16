5de2e52fe6e1c349dc3dc58a7349c9b374bb5af0
VaskinaYana <vaskinayana866@gmail.com>
2026-03-16 20:43:40 +0500

    Add files via upload

---
+++ 2.Задание 1/Program.fs
@@ -0,0 +1,38 @@
+﻿let rec readNumbers accumulator =
+    let input = System.Console.ReadLine()
+    
+    match System.Int32.TryParse(input) with
+    | (true, number) when number >= 1 && number <= 9 ->
+        readNumbers (number :: accumulator)
+    | _ ->
+        accumulator
+
+let toRoman number =
+    match number with
+    | 1 -> "I"
+    | 2 -> "II"
+    | 3 -> "III"
+    | 4 -> "IV"
+    | 5 -> "V"
+    | 6 -> "VI"
+    | 7 -> "VII"
+    | 8 -> "VIII"
+    | 9 -> "IX"
+    | _ -> failwith "Неподдерживаемое число"
+
+[<EntryPoint>]
+let main argv =
+    printfn "Введите числа от 1 до 9 (для завершения введите любой другой символ):"
+
+    let originalList = 
+        readNumbers []
+        |> List.rev
+    
+    let romanList =
+        originalList
+        |> List.map toRoman
+
+    printfn "Начальный список: %A" originalList
+    printfn "Результат: %A" romanList
+    
+    0
---
+++ 2.Задание 2/Program.fs
@@ -0,0 +1,25 @@
+﻿let rec readDigits accumulator =
+    let input = System.Console.ReadLine()
+    
+    match System.Int32.TryParse(input) with
+    | (true, digit) when digit >= 0 && digit <= 9 ->
+        readDigits (digit :: accumulator)
+    | _ ->
+        accumulator
+
+let digitsToNumber digits =
+    digits
+    |> List.fold (fun accumulated digit -> accumulated * 10 + digit) 0
+
+[<EntryPoint>]
+let main argv =
+    printfn "Введите десятичные цифры (для завершения введите любой другой символ):"
+    
+    let originalDigits = 
+        readDigits []
+        |> List.rev
+
+    printfn "Введённые цифры: %A" originalDigits
+    printfn "Полученное число: %d" (digitsToNumber originalDigits)
+    
+    0
